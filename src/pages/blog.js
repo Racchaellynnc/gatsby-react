75183a259fa9f14ca79a95874818516924da9e7d
rachael <rachael.coder@gmail.com>
2020-04-20 02:19:14 -0700

    blog update

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -10,8 +10,8 @@ return (
         <PrimaryLayout column="col-xs-6">
             {data.allWordpressPost.nodes.map(node => (
             <Post 
-                // alt={node.featured_media.slug}
-                // image={node.featured_media.source_url}
+                alt={node.featured_media.slug}
+                image={node.featured_media.source_url}
                 title={node.title}
                 excerpt={node.excerpt}
                 readMore={node.slug}
